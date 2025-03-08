d8f5e1452d1faccb165174752ffb13bebbf63f53
akarahdev <endisticbutitsforgit@gmail.com>
2025-03-07 22:41:30 -0500

    feat: remove neuron network flattening (this may be added back later)

--- src/networks/mod.rs
+++ src/networks/mod.rs
@@ -1,10 +1,6 @@
 mod perceptron;
 pub use perceptron::*;
 
-pub trait NeuralNetwork<const I: usize, const O: usize, F: Fn(f16) -> f16> {
+pub trait NeuralNetwork<const I: usize, const O: usize> {
     fn feed(&mut self, arr: &[f16; I]) -> [f16; O];
-
-    const FLATTENED_SIZE: usize;
-    fn flatten(&self) -> [f16; Self::FLATTENED_SIZE];
-    fn unflatten(flattened: [f16; Self::FLATTENED_SIZE], activation_function: F) -> Self;
 }
--- src/networks/perceptron.rs
+++ src/networks/perceptron.rs
@@ -17,7 +17,7 @@ impl<const I: usize, F: Fn(f16) -> f16> Perceptron<I, F> {
     }
 }
 
-impl<const I: usize, F: Fn(f16) -> f16> NeuralNetwork<I, 1, F> for Perceptron<I, F> {
+impl<const I: usize, F: Fn(f16) -> f16> NeuralNetwork<I, 1> for Perceptron<I, F> {
     fn feed(&mut self, arr: &[f16; I]) -> [f16; 1] {
         let mut sum = 0.0;
         for item in arr.iter().enumerate() {
@@ -25,32 +25,6 @@ impl<const I: usize, F: Fn(f16) -> f16> NeuralNetwork<I, 1, F> for Perceptron<I,
         }
         sum += self.bias;
         [(self.activation_function)(sum)]
-    }
-
-    const FLATTENED_SIZE: usize = I + 1;
-
-    fn flatten(&self) -> [f16; Self::FLATTENED_SIZE] {
-        let mut arr = [0.0; Self::FLATTENED_SIZE];
-        for item in self.weights.iter().enumerate() {
-            arr[item.0] = *item.1;
-        }
-        arr[arr.len() - 1] = self.bias;
-        arr
-    }
-
-    fn unflatten(flattened: [f16; Self::FLATTENED_SIZE], activation_function: F) -> Self
-    where
-        Self: Sized,
-    {
-        let weights_len = flattened.len() - 1;
-        let bias = flattened[weights_len];
-        let mut weights = [0.0; I];
-        weights[..weights_len].copy_from_slice(&flattened[..weights_len]);
-        Perceptron {
-            bias,
-            weights,
-            activation_function,
-        }
     }
 }
 
@@ -113,17 +87,5 @@ mod tests {
 
         let output = perceptron.feed(&[0.0, 0.0]);
         assert_eq!(output[0], 0.0, "False | False != False");
-    }
-
-    #[test]
-    fn perceptron_reflatten() {
-        let closure = |x| if x > 0.0 { 1.0 } else { 0.0 };
-        let perceptron = Perceptron::new([1.0, 1.0], -1.5, closure);
-
-        let flattened = perceptron.flatten();
-        let unflattened = Perceptron::unflatten(flattened, closure);
-
-        assert_eq!(perceptron.weights, unflattened.weights);
-        assert_eq!(perceptron.bias, unflattened.bias);
     }
 }
